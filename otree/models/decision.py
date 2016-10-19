05052a4fa072b785993853a9d79546f5968a02d5
James Pettit <jpettit@google.com>
2016-10-19 23:41:12 +0000

    Tabs to spaces.

--- otree/models/decision.py
+++ otree/models/decision.py
@@ -5,16 +5,16 @@ from otree.db import models
 
 class Decision(models.Model):
 
-	class Meta:
-		app_label = "otree"
-		# if i don't set this, it could be in an unpredictable order
-		ordering = ['pk']
+  class Meta:
+    app_label = "otree"
+    # if i don't set this, it could be in an unpredictable order
+    ordering = ['pk']
 
-	timestamp = models.DateTimeField(auto_now_add=True)
-	component = models.CharField(max_length=100, null=False)
-	session = models.CharField(max_length=100, null=False)
-	subsession = models.IntegerField(null=True)
-	round = models.IntegerField(null=False)
-	group = models.IntegerField(null=False)
-	participant = models.ForeignKey('otree.Participant', null=False)
-	decision = models.JSONField(null=False)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  component = models.CharField(max_length=100, null=False)
+  session = models.CharField(max_length=100, null=False)
+  subsession = models.IntegerField(null=True)
+  round = models.IntegerField(null=False)
+  group = models.IntegerField(null=False)
+  participant = models.ForeignKey('otree.Participant', null=False)
+  decision = models.JSONField(null=False)
--- otree/models/log.py
+++ otree/models/log.py
@@ -5,15 +5,15 @@ from otree.db import models
 
 class LogEvent(models.Model):
 
-	class Meta:
-	    app_label = "otree"
-	    # if i don't set this, it could be in an unpredictable order
-	    ordering = ['pk']
+  class Meta:
+      app_label = "otree"
+      # if i don't set this, it could be in an unpredictable order
+      ordering = ['pk']
 
-	timestamp = models.DateTimeField(auto_now_add=True)
-	session = models.CharField(max_length=100, null=False)
-	subsession = models.IntegerField(null=False)
-	round = models.IntegerField(null=False)
-	group = models.IntegerField(null=False)
-	participant = models.ForeignKey('otree.Participant', null=False)
-	event = models.JSONField(null=False)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  session = models.CharField(max_length=100, null=False)
+  subsession = models.IntegerField(null=False)
+  round = models.IntegerField(null=False)
+  group = models.IntegerField(null=False)
+  participant = models.ForeignKey('otree.Participant', null=False)
+  event = models.JSONField(null=False)
