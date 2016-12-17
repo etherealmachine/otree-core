4713d769ed6f3e7c7f5bf1c6a2e18aea7975387f
Morgan Grant <gramorgan@gmail.com>
2016-12-17 12:44:27 -0800

    added app and page to tickfile output

--- otree/firebase/ticks.py
+++ otree/firebase/ticks.py
@@ -3,21 +3,25 @@ def collect_ticks(decisions):
     for decision in decisions:
         ticks.append({
             'timestamp': decision.timestamp,
-            'component': decision.component,
             'session': decision.session,
+            'app': decision.app,
             'subsession': decision.subsession,
             'round': decision.round,
             'group': decision.group,
+            'page': decision.page,
+            'component': decision.component,
             'participant': decision.participant.code,
             'decision': decision.decision
         })
     return [
         'timestamp',
-        'component',
         'session',
+        'app',
         'subsession',
         'round',
         'group',
+        'page',
+        'component',
         'participant',
         'decision'
     ], ticks
